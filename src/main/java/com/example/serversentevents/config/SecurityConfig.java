587507b9da6a18ffb4832c71adceb713ded0315e
raychong <raychongtk@gmail.com>
2022-03-19 18:55:16 +0800

    update dependency

--- src/main/java/com/example/serversentevents/config/SecurityConfig.java
+++ src/main/java/com/example/serversentevents/config/SecurityConfig.java
@@ -1,8 +1,8 @@
 package com.example.serversentevents.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -11,7 +11,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 /**
  * @author raychong
  */
-@EnableWebSecurity
+@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
