0b0d251b334aba56ee20b7b4bde6165ab3cbb2b8
raychong <raychongtk@gmail.com>
2022-03-19 19:22:16 +0800

    add content type

--- src/main/java/com/example/serversentevents/controller/ExchangeController.java
+++ src/main/java/com/example/serversentevents/controller/ExchangeController.java
@@ -3,6 +3,7 @@ package com.example.serversentevents.controller;
 import com.example.serversentevents.apipayload.ExchangeRates;
 import com.example.serversentevents.event.UpdateExchangeRateEventListener;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
@@ -15,7 +16,7 @@ public class ExchangeController {
     @Autowired
     UpdateExchangeRateEventListener updateExchangeRateEventListener;
 
-    @GetMapping(value = "/api/exchange")
+    @GetMapping(value = "/api/exchange", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     public Flux<ExchangeRates> getExchangeRates() {
         return updateExchangeRateEventListener.createLatestExchangeRates();
     }
