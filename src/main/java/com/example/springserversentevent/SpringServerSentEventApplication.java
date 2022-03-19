ffbd4deac89a645277d706bfb70bd462b5704268
raychong <raychongtk@gmail.com>
2022-03-19 13:23:46 +0800

    init spring boot

---
+++ src/main/java/com/example/springserversentevent/SpringServerSentEventApplication.java
@@ -0,0 +1,13 @@
+package com.example.springserversentevent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringServerSentEventApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringServerSentEventApplication.class, args);
+    }
+
+}
